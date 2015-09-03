47e116d6e2da9bf0f1f451d96f835515ceae8880
Antonio Altieri <antonio.danilo92@gmail.com>
2015-09-03 12:27:33 +0200

    Test mp3 Audio send

--- src/test/java/audiotest.java
+++ src/test/java/audiotest.java
@@ -1,31 +1,45 @@
 
 
 import java.io.File;
+import java.net.URL;
 
+import com.asdamp.telegram.botapi.requests.OptionalArgs;
+import com.asdamp.telegram.botapi.requests.SendAudioRequest;
+import com.asdamp.telegram.botapi.types.Message;
 import org.junit.*;
+import static org.junit.Assert.*;
+
 
 
 import com.asdamp.telegram.botapi.requests.ApiRequestExecutor;
-import com.asdamp.telegram.botapi.requests.SendAudioRequest;
 import com.asdamp.telegram.botapi.requests.TelegramApi;
-import com.asdamp.telegram.botapi.types.Message;
+
 
 public class audiotest {
-	protected final static String TOKEN = "124217141:AAFMLhB5MWvHWaFRi9xpyVBIAgBkSEanEbM";
+	protected final static String TOKEN = TestToken.TOKEN;
     protected TelegramApi api;
     protected ApiRequestExecutor requestExecutor;
+    File audioFile;
 
     @Before
     public void setUp() throws Exception {
         api = new TelegramApi(TOKEN);
+        assertNotNull(api);
         requestExecutor = ApiRequestExecutor.getInstance();
+        assertNotNull(requestExecutor);
+        URL url=this.getClass().getResource("TestAudioFile.mp3");
+        audioFile=new File(url.getFile());
+        assertTrue(audioFile.exists());
     }
     
     @Test
     public void audioSend(){
-    	SendAudioRequest sar=new SendAudioRequest(14827206,new File("C:/Users/Antonio/Music/JovaBot/Sabato-ballare.mp3"));
+        OptionalArgs option=new OptionalArgs();
+        option.performer("Asdamp");
+        option.title("TestSong");
+    	SendAudioRequest sar=new SendAudioRequest(TestToken.chat_id,audioFile,option);
     	Message msg=requestExecutor.execute(api, sar);
-    	System.out.println(msg.getAudio().getFileId());
+        assertTrue(msg.getAudio()!=null);
     }
 
 }
